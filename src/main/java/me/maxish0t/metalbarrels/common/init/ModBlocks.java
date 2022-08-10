e860fd44b85cfdd7af32ca94beeac5662ab31258
MaxSaleh <maxsaleh2003@gmail.com>
2022-08-09 18:53:05 -0600

    Updated block properties

--- src/main/java/me/maxish0t/metalbarrels/common/init/ModBlocks.java
+++ src/main/java/me/maxish0t/metalbarrels/common/init/ModBlocks.java
@@ -5,6 +5,7 @@ import me.maxish0t.metalbarrels.common.block.MetalBarrelBlock;
 import me.maxish0t.metalbarrels.common.block.entity.MetalBarrelBlockEntity;
 import me.maxish0t.metalbarrels.MetalBarrels;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,55 +16,51 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, MetalBarrels.MODID);
 
+    private static final BlockBehaviour.Properties metal = BlockBehaviour.Properties
+            .of(Material.METAL)
+            .strength(2.5F, 6.0F)
+            .requiresCorrectToolForDrops()
+            .sound(SoundType.METAL);
+
+    private static final BlockBehaviour.Properties obsidian = BlockBehaviour.Properties
+            .of(Material.STONE)
+            .strength(15.0F, 6000.0F)
+            .requiresCorrectToolForDrops()
+            .sound(SoundType.NETHER_ORE);
+
     /**
      * Metal Barrels Block Registry.
      */
 
     public static final RegistryObject<Block> COPPER_BARREL = REGISTER.register(
-            "copper_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "copper", MetalBarrelBlockEntity::copper)
+            "copper_barrel", () -> new MetalBarrelBlock(metal, "copper", MetalBarrelBlockEntity::copper)
     );
 
     public static final RegistryObject<Block> IRON_BARREL = REGISTER.register(
-            "iron_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "iron", MetalBarrelBlockEntity::iron)
+            "iron_barrel", () -> new MetalBarrelBlock(metal, "iron", MetalBarrelBlockEntity::iron)
     );
 
     public static final RegistryObject<Block> SILVER_BARREL = REGISTER.register(
-            "silver_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "silver", MetalBarrelBlockEntity::silver)
+            "silver_barrel", () -> new MetalBarrelBlock(metal, "silver", MetalBarrelBlockEntity::silver)
     );
 
     public static final RegistryObject<Block> GOLD_BARREL = REGISTER.register(
-            "gold_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "gold", MetalBarrelBlockEntity::gold)
+            "gold_barrel", () -> new MetalBarrelBlock(metal, "gold", MetalBarrelBlockEntity::gold)
     );
 
     public static final RegistryObject<Block> DIAMOND_BARREL = REGISTER.register(
-            "diamond_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "diamond", MetalBarrelBlockEntity::diamond)
+            "diamond_barrel", () -> new MetalBarrelBlock(metal, "diamond", MetalBarrelBlockEntity::diamond)
     );
 
     public static final RegistryObject<Block> OBSIDIAN_BARREL = REGISTER.register(
-            "obsidian_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "obsidian", MetalBarrelBlockEntity::obsidian)
+            "obsidian_barrel", () -> new MetalBarrelBlock(obsidian, "obsidian", MetalBarrelBlockEntity::obsidian)
     );
 
     public static final RegistryObject<Block> NETHERITE_BARREL = REGISTER.register(
-            "netherite_barrel", () -> new MetalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "netherite", MetalBarrelBlockEntity::netherite)
+            "netherite_barrel", () -> new MetalBarrelBlock(obsidian, "netherite", MetalBarrelBlockEntity::netherite)
     );
 
     public static final RegistryObject<Block> CRYSTAL_BARREL = REGISTER.register(
-            "crystal_barrel", () -> new CrystalBarrelBlock(BlockBehaviour.Properties.of(Material.AIR)
-                    .requiresCorrectToolForDrops().strength(2.5F),
-                    "crystal", MetalBarrelBlockEntity::crystal)
+            "crystal_barrel", () -> new CrystalBarrelBlock(metal.noOcclusion(), "crystal", MetalBarrelBlockEntity::crystal)
     );
 }
